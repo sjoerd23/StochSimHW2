30f6585de051e7e2a478ca9c1c864595ae7d6964
Coen Lenting <coen@Coens-MacBook-Pro.local>
2020-11-30 16:33:10 +0100

    updated

--- DES_coen.py
+++ DES_coen.py
@@ -2,7 +2,7 @@ import simpy
 import numpy as np
 import matplotlib.pyplot as plt
 import tqdm
-
+import pandas as pd 
 
 class Queue(object):
 	"""
@@ -55,15 +55,15 @@ def person(env, name, queue, mu, serv_t_dist):
 
 	# compute the task time
 	if serv_t_dist == "M":
-		task_time = -np.log(np.random.random())
+		task_time = -np.log(np.random.random()) / 1
 	elif serv_t_dist == "D":
 		task_time = 1
 	elif serv_t_dist == "LT":
 		x = np.random.random()
 		if x < 0.75:
-			task_time = -np.log(np.random.random()) / 1
+			task_time = -np.log(np.random.random()) * 0.5
 		else:
-			task_time = -np.log(np.random.random()) / 5
+			task_time = -np.log(np.random.random()) * 2.5
 	else:
 		raise ValueError("Invalid type of service time distribution")
 	# print("{} arrives at queue at {} with task time {:.2f}".format(name, arrival, task_time))
@@ -126,22 +126,16 @@ def simulation(servers, rho, priority, max_persons, serv_t_dist):
 
 	env = simpy.Environment()
 
-	# if serv_t_dist == "M":
-	# 	lamda_load = l * servers
-	# elif serv_t_dist == "D":
-	# 	lamda_load = l * servers
-	# elif serv_t_dist == "LT":
-	# 	lamda_load = l * ((5 / 4) + (3 / 4)) * servers / 2
+	mu = 1
+	lamda_eff = mu * rho * servers
+	
 
-	mu_d = {"M": 1, "D": 1, "LT" : 2}
-	lamda_eff = mu_d[serv_t_dist] *  rho * servers
-	mu = mu_d[serv_t_dist]
 	env.process(setup(env, servers,  lamda_eff, mu, priority, max_persons, serv_t_dist))
 
 	env.run(None)
 	return wt 
 
-def compute_avg_wt(servers, rho_l, max_persons, sims, priority=False, serv_t_dist = "M"):
+def compute_avg_wt(servers, rho_l, max_persons, sims, priority=False, serv_t_dist = "M", truncation = 0):
 	"""
 	compute the average waiting time for a scala of
 	servers and values for lamda and plot the results
@@ -161,22 +155,28 @@ def compute_avg_wt(servers, rho_l, max_persons, sims, priority=False, serv_t_dis
 			number of simulation for each combination (servers, lamda)
 	"""
 
+	df = pd.DataFrame()
 
 
 	for n_servers in servers:
 		server_wt = []
+		conf_int = []
 		for rho in rho_l:
+			print(n_servers, rho)
 			mean_wt = []
-			for _ in range(sims):
+			for _ in tqdm.tqdm(range(sims)):
 				waiting_time = simulation(n_servers, rho, priority, max_persons, serv_t_dist)
-				mean_wt.append(np.mean(waiting_time))
-
+				mean_wt.append(np.mean(waiting_time[truncation:]))
+			new_row = {"servers":n_servers, "rho":rho, "mean_wt":mean_wt}
+			df = df.append(new_row, ignore_index = True)
 			server_wt.append(np.mean(mean_wt))
+			conf_int.append(1.96 * np.std(mean_wt, ddof=1) / np.sqrt(sims))
 		
-		plt.plot(rho_l, server_wt, label='{} servers, servicet dist = {}'.format(n_servers, serv_t_dist))
+		plt.errorbar(rho_l, server_wt, yerr = conf_int, label='{} servers, servicet dist = {}'.format(n_servers, serv_t_dist))
 		plt.xlabel("rho [-]")
 		plt.ylabel("mean waiting time [-]")
 	plt.legend()
+	return df
 
 def wt_distribution(servers, rho_l, max_persons, sims_l, priority=False, serv_t_dist = "M"):
 	"""
@@ -204,11 +204,19 @@ def wt_distribution(servers, rho_l, max_persons, sims_l, priority=False, serv_t_
 			print(max_p)
 			for sims in sims_l:
 				waiting_time_dist = []
+				waiting_time_dist_trunc = []
 				for _ in range(sims):
 					waiting_time = simulation(servers, rho, priority, max_p, serv_t_dist)
 					waiting_time_dist.append(np.mean(waiting_time))
+					
+					waiting_time_dist_trunc.append(np.mean(waiting_time[int(max_p * 0.3):]))
 
-				plt.hist(waiting_time_dist, label="max_p: {}, sims: {}".format(max_p, sims), alpha = 0.3, density=True)
+				# plt.hist(waiting_time_dist, label="max_p: {}".format(max_p), alpha = 0.3, density=True)
+				plt.hist(waiting_time_dist, label="rho: {}".format(rho), alpha = 0.3, density=True)
+				
+				# plt.hist(waiting_time_dist_trunc, label="max_p: {}, truncated".format(max_p), alpha = 0.3, density=False)
+				plt.xlabel("average waiting time [-]", fontsize = 16)
+				plt.ylabel("density [-]", fontsize = 16)
 	# 		server_wt.append(np.mean(mean_wt))
 		
 	# 	plt.plot(lamda, server_wt, label='{} servers'.format(n_servers))
@@ -276,17 +284,22 @@ if __name__ == "__main__":
 	global wt
 	wt = []
 
-	# servers_l = [1, 2, 4]
-	# rho_l = np.linspace(0.6, 1.2, 8)
-	# max_persons = 500
-	# sims = 10
+	servers_l = [1, 2, 4]
+	rho_l1 = np.linspace(0.1, 0.5, 5)
+	rho_l2 = np.linspace(0.6, 0.95, 15)
+	rho_l = np.concatenate((rho_l1, rho_l2), axis = 0)	
+	max_persons = 20000
+	sims = 100
+	truncation = 6000
 
-	# # M/M/n queue without priority
-	# compute_avg_wt(servers_l, rho_l, max_persons, sims)
+	# M/M/n queue without priority
+	data = compute_avg_wt(servers_l, rho_l, max_persons, sims, truncation = truncation)
+	data.to_csv("M124-p{}-t{}-sims{}_2.csv".format(max_persons, truncation, sims))
 
-	# # # M/M/1 queue with shortest job first scheduling
-	# compute_avg_wt([1], rho_l, max_persons, sims, priority=True)
-	
+
+	# # M/M/1 queue with shortest job first scheduling
+	data = compute_avg_wt([1], rho_l, max_persons, sims, priority=True, truncation = truncation)
+	data.to_csv("M124-p{}-t{}-sims{}-prio_2.csv".format(max_persons, truncation, sims))
 
 	# servers_l = [1]
 	# rho_l = [0.9]
@@ -296,21 +309,22 @@ if __name__ == "__main__":
 
 
 
-	servers_l = [1, 2, 4]
-	rho_l = np.linspace(0.6, 1, 6)
-	max_persons = 200
-	sims = 50
+	# servers_l = [1, 2, 4]
+	# rho_l = np.linspace(0.6, 1, 6)
+	# max_persons = 200
+	# sims = 50
 
 	# M/M/n queue without priority
-	plt.figure()
-	compute_avg_wt(servers_l, rho_l, max_persons, sims, serv_t_dist = "M")
+	# plt.figure()
+	# compute_avg_wt(servers_l, rho_l, max_persons, sims, serv_t_dist = "M")
 
-	plt.figure()
-	compute_avg_wt(servers_l, rho_l, max_persons, sims, serv_t_dist = "D")
+	# plt.figure()
+	# data = compute_avg_wt(servers_l, rho_l, max_persons, sims, serv_t_dist = "D", truncation = truncation)
+	# data.to_csv("D124-p{}-t{}-sims{}.csv".format(max_persons, truncation, sims))
 
-	plt.figure()
-	compute_avg_wt(servers_l, rho_l, max_persons, sims, serv_t_dist = "LT")
-
+	# plt.figure()
+	# data = compute_avg_wt(servers_l, rho_l, max_persons, sims, serv_t_dist = "LT")
+	# data.to_csv("LT124-p{}-t{}-sims{}.csv".format(max_persons, truncation, sims))
 
 
 	##################################################################################
@@ -318,13 +332,15 @@ if __name__ == "__main__":
 	# used to determine whether the system has entered the steady state
 	# is dependent on sims, rho and max_persons
 	##################################################################################
-	# global wt
-	# wt = []
-	# servers = 1
-	# rho_l = [0.1]9
-	# max_persons = [100, 1000]
-	# sims = [500]
+
+	# servers = 4
+	# rho_l = [0.5, 0.7, 0.95]
+	# max_persons = [1000]
+	# sims = [100]
 	# wt_distribution(servers, rho_l, max_persons, sims, serv_t_dist = "M")
+	# plt.xlim(0,6)
+	# plt.legend(fontsize=16)
+
 	plt.show()
 
 
